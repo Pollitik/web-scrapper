3da2c55c688558a7641bfc47768dafa38637e8ea
satvik-1203 <satvikgsu@gmail.com>
2022-01-28 17:25:14 -0500

    added puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,174 +1,33 @@
-import axios from "axios";
-import { JSDOM } from "jsdom";
+import puppeteer from "puppeteer";
+import { google } from "googleapis";
+const sheets = google.sheets("v4");
 
-// @ts-ignore
 async function main() {
-  const res = await axios.get(
-    "https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_New_Zealand_general_election#Government_approval_rating"
-  );
-
-  const {
-    window: {
-      document: { body },
-    },
-  } = new JSDOM(res.data);
-
-  const tables = body.querySelectorAll("table");
-
-  tables.forEach((table) => {
-    const tableHeadElements = table.querySelectorAll("th");
-
-    let found = false;
-
-    const indices: any = {
-      Date: 0,
-      "Right direction": 0,
-      "Wrong direction": 0,
-      "Sample size": 0,
-      "Do not know": 0,
-    };
-
-    const dataMap = new Map<number, Array<string>>();
-
-    tableHeadElements.forEach((el) => {
-      if (el.textContent?.trim() == "Right direction") {
-        found = true;
-        tableHeadElements.forEach((el, index) => {
-          const supElement = el.querySelector("sup");
-          if (supElement) el.removeChild(supElement);
-
-          if (indices.hasOwnProperty(el.textContent?.trim())) {
-            indices[el.textContent!.trim()] = index;
-            dataMap.set(index, []);
-          }
-        });
-      }
-    });
-
-    if (!found) return;
-
-    // Found the right table
-    // extract the numbers in the right indices
-    // and put them accordingly in an array
-
-    const tableBody = table.querySelector("tbody")!;
-    const bodyRows = tableBody.querySelectorAll("tr");
-
-    bodyRows.forEach((row) => {
-      const tdArray = row.querySelectorAll("td");
-
-      tdArray.forEach((td, index) => {
-        const dataArr = dataMap.get(index);
-
-        if (!dataArr) return;
-
-        const boldTag = td.querySelector("b");
-
-        let content = (boldTag ? boldTag.textContent : td.textContent) || "";
-
-        dataArr.push(content.trim());
-      });
-    });
-
-    console.table(indices);
-    console.table(dataMap);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://morningconsult.com/global-leader-approval/", {
+    waitUntil: "domcontentloaded",
   });
-}
 
-// main();
-
-interface IIndices extends Object {
-  "Date(s)conducted": number;
-  Approve: number;
-  Disapprove: number;
-  "Sample size": number;
-}
-
-async function main2() {
-  const res = await axios.get(
-    "https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Polish_parliamentary_election#President_Andrzej_Duda"
-  );
-
-  const {
-    window: {
-      document: { body },
-    },
-  } = new JSDOM(res.data);
-
-  const tables = body.querySelectorAll("table");
-
-  tables.forEach((table) => {
-    const tableHeadElements = table.querySelectorAll("th");
-
-    let found = false;
-
-    const indices: IIndices = {
-      "Date(s)conducted": 0,
-      Approve: 0,
-      Disapprove: 0,
-      "Sample size": 0,
-    };
-
-    const dataMap = new Map<number, Array<string>>();
-
-    if (tableHeadElements) {
-      tableHeadElements.forEach((el) => {
-        if (el.textContent?.trim() == "Approve") {
-          found = true;
-          tableHeadElements.forEach((el, index) => {
-            // refractor this code to remove any child instead of hardcoded child
-            const supElement = el.querySelector("sup");
-            if (supElement) el.removeChild(supElement);
-
-            const spanElement = el.querySelector("span");
-            if (spanElement) el.removeChild(spanElement);
-
-            const brElement = el.querySelector("br");
-
-            try {
-              if (brElement) el.removeChild(brElement);
-            } catch {}
-
-            if (indices.hasOwnProperty(el.textContent?.trim()!)) {
-              // @ts-ignore
-
-              indices[el.textContent!.trim()] = index;
-              dataMap.set(index, []);
-            }
-          });
-        }
-      });
-    } else {
-    }
-
-    if (!found) return;
-
-    // Found the right table
-    // extract the numbers in the right indices
-    // and put them accordingly in an array
-
-    const tableBody = table.querySelector("tbody")!;
-    const bodyRows = tableBody.querySelectorAll("tr");
-
-    bodyRows.forEach((row) => {
-      const tdArray = row.querySelectorAll("td");
-
-      tdArray.forEach((td, index) => {
-        const dataArr = dataMap.get(index);
-
-        if (!dataArr) return;
-
-        const boldTag = td.querySelector("b");
-
-        let content = (boldTag ? boldTag.textContent : td.textContent) || "";
-
-        dataArr.push(content.trim());
-      });
-    });
-
-    console.table(indices);
-    console.table(dataMap);
+  const data = await page.evaluate(() => {
+    // @ts-ignore
+    return mc_timeline_filterable;
   });
+  console.log(data["1"][0][0]);
+  // const auth = new google.auth.GoogleAuth({
+  //   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  //   credentials: {},
+  // });
+  // const authClient = await auth.getClient();
+  // google.options({ auth: authClient });
+  // console.log("Worked");
+
+  browser.close();
 }
 
-main2();
+main();
+// https://docs.google.com/spreadsheets/d/1w7tRoI3AXAokaGRu9PAWk9K9Us10YbCyYOFqWy6SQbQ/edit?usp=sharing
+
+// sheetid 1w7tRoI3AXAokaGRu9PAWk9K9Us10YbCyYOFqWy6SQbQ
+
+//spreadsheets.google.com/feeds/worksheets/spreadsheetID/public/basic?alt=json
---
+++ src/webScrappers/wiki.ts
@@ -0,0 +1,190 @@
+import axios from "axios";
+import { JSDOM } from "jsdom";
+
+// @ts-ignore
+async function main() {
+  const res = await axios.get(
+    "https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_New_Zealand_general_election#Government_approval_rating"
+  );
+
+  const {
+    window: {
+      document: { body },
+    },
+  } = new JSDOM(res.data);
+
+  const tables = body.querySelectorAll("table");
+
+  tables.forEach((table) => {
+    const tableHeadElements = table.querySelectorAll("th");
+
+    let found = false;
+
+    const indices: any = {
+      Date: 0,
+      "Right direction": 0,
+      "Wrong direction": 0,
+      "Sample size": 0,
+      "Do not know": 0,
+    };
+
+    const dataMap = new Map<number, Array<string>>();
+
+    tableHeadElements.forEach((el) => {
+      if (el.textContent?.trim() == "Right direction") {
+        found = true;
+        tableHeadElements.forEach((el, index) => {
+          const supElement = el.querySelector("sup");
+          if (supElement) el.removeChild(supElement);
+
+          if (indices.hasOwnProperty(el.textContent?.trim())) {
+            indices[el.textContent!.trim()] = index;
+            dataMap.set(index, []);
+          }
+        });
+      }
+    });
+
+    if (!found) return;
+
+    // Found the right table
+    // extract the numbers in the right indices
+    // and put them accordingly in an array
+
+    const tableBody = table.querySelector("tbody")!;
+    const bodyRows = tableBody.querySelectorAll("tr");
+
+    bodyRows.forEach((row) => {
+      const tdArray = row.querySelectorAll("td");
+
+      tdArray.forEach((td, index) => {
+        const dataArr = dataMap.get(index);
+
+        if (!dataArr) return;
+
+        const boldTag = td.querySelector("b");
+
+        let content = (boldTag ? boldTag.textContent : td.textContent) || "";
+
+        dataArr.push(content.trim());
+      });
+    });
+
+    console.table(indices);
+    console.table(dataMap);
+  });
+}
+
+// main();
+
+// @ts-ignore
+
+interface IIndices extends Object {
+  "Date(s)conducted": number;
+  Approve: number;
+  Disapprove: number;
+  "Sample size": number;
+}
+
+interface props {
+  url: string;
+  indices: any;
+  approve: string;
+}
+
+async function main2({ approve, indices, url }: props) {
+  const res = await axios.get(url);
+
+  const {
+    window: {
+      document: { body },
+    },
+  } = new JSDOM(res.data);
+
+  const tables = body.querySelectorAll("table");
+
+  tables.forEach((table) => {
+    const tableHeadElements = table.querySelectorAll("th");
+
+    let found = false;
+
+    // const indices: any = {
+    //   "Date(s)conducted": 0,
+    //   Approve: 0,
+    //   Disapprove: 0,
+    //   "Sample size": 0,
+    // };
+
+    const dataMap = new Map<number, Array<string>>();
+
+    if (tableHeadElements) {
+      tableHeadElements.forEach((el) => {
+        if (el.textContent?.trim() == approve) {
+          found = true;
+          tableHeadElements.forEach((el, index) => {
+            // refractor this code to remove any child instead of hardcoded child
+            const supElement = el.querySelector("sup");
+            if (supElement) el.removeChild(supElement);
+
+            const spanElement = el.querySelector("span");
+            if (spanElement) el.removeChild(spanElement);
+
+            const brElement = el.querySelector("br");
+
+            try {
+              if (brElement) el.removeChild(brElement);
+            } catch {}
+
+            if (indices.hasOwnProperty(el.textContent?.trim()!)) {
+              // @ts-ignore
+
+              indices[el.textContent!.trim()] = index;
+              dataMap.set(index, []);
+            }
+          });
+        }
+      });
+    } else {
+    }
+
+    if (!found) return;
+
+    // Found the right table
+    // extract the numbers in the right indices
+    // and put them accordingly in an array
+
+    const tableBody = table.querySelector("tbody")!;
+    const bodyRows = tableBody.querySelectorAll("tr");
+
+    bodyRows.forEach((row) => {
+      const tdArray = row.querySelectorAll("td");
+
+      tdArray.forEach((td, index) => {
+        const dataArr = dataMap.get(index);
+
+        if (!dataArr) return;
+
+        const boldTag = td.querySelector("b");
+
+        let content = (boldTag ? boldTag.textContent : td.textContent) || "";
+
+        dataArr.push(content.trim());
+      });
+    });
+
+    console.table(indices);
+    console.table(dataMap);
+  });
+}
+
+main2({
+  url: "https://en.wikipedia.org/wiki/Opinion_polling_for_the_next_Polish_parliamentary_election#President_Andrzej_Duda",
+  approve: "Approve",
+  indices: {
+    "Date(s)conducted": 0,
+    "Sample size": 0,
+    Approve: 0,
+    Disapprove: 0,
+    "Pollster/client": 0,
+  },
+});
